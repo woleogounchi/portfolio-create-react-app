e3299ee20e0d57bf6bdd75f7a64d9a23dd517bc2
Kolawole Ogounchi <kolawoleogounchi@outlook.com>
2020-05-17 03:41:52 -0400

    Updates

---
+++ precache-manifest.662f389d3dbfe234a13abe03365eed5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da833bdac38e4ff4600908774aaff30b",
+    "url": "/woleogounchi/portfolio-create-react-app/index.html"
+  },
+  {
+    "revision": "fe21e675724557e473fa",
+    "url": "/woleogounchi/portfolio-create-react-app/static/css/main.745342b2.chunk.css"
+  },
+  {
+    "revision": "d2eb3afe6758c5928468",
+    "url": "/woleogounchi/portfolio-create-react-app/static/js/2.b566c1c1.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/woleogounchi/portfolio-create-react-app/static/js/2.b566c1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe21e675724557e473fa",
+    "url": "/woleogounchi/portfolio-create-react-app/static/js/main.34e7f31d.chunk.js"
+  },
+  {
+    "revision": "68996052c5bbe15e8db7",
+    "url": "/woleogounchi/portfolio-create-react-app/static/js/runtime-main.be58f128.js"
+  }
+]);
